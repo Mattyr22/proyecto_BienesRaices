56c9743beaed003f654eb99eefe537f83b1385d6
Mattyr22 <mattyrosales@gmail.com>
2022-07-11 18:40:47 -0300

    Proyecto de pagina de bienes raices

---
+++ src/js/app.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    eventListeners();
+
+    darkMode();
+});
+
+function eventListeners() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    mobileMenu.addEventListener('click', navegacionResponsive);
+}
+
+function navegacionResponsive() {
+    const navegacion = document.querySelector('.navegacion');
+
+    navegacion.classList.toggle('mostrar')
+    // if(navegacion.classList.contains('mostrar')) {
+    //     navegacion.classList.remove('mostrar');
+    // }else {
+    //     navegacion.classList.add('mostrar');
+    // }
+}
+
+function darkMode() {
+    const prefiereDarkMode = window.matchMedia('(preferns-color-scheme:darck)');
+    //console.log(prefiereDarkMode.matches);
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function () {
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }    
+    });
+    
+    const botonDarckMode = document.querySelector('.dark-mode-boton')
+
+    botonDarckMode.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode')
+    })
+}
